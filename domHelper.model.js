f9195d3df2ecc16e23decd1226297e0d74943d7a
Tim <tim@fostle.com>
2009-11-15 21:13:02 +1300

    Initiated DomHelper model

---
+++ domHelper.model.js
@@ -0,0 +1,11 @@
+#ifndef _MODEL_STORAGE
+#define _MODEL_STORAGE
+/*
+ * DomHelper model: Helps to build UI
+ */
+
+var ModelDomHelper = new Class({
+	Extends: Model,
+	constructor: ModelDomHelper
+});
+#endif
